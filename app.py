14e11be9062361c9f4c7e8a7c4e8844cd8c7edbe
Madhan425 <madhanofficials17@gmail.com>
2026-09-26 10:18:00 +0530

    added flask

--- app.py
+++ app.py
@@ -1,34 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+# Static route
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-@app.route("/register", methods=["GET", "POST"])
-def register():
-
-    if request.method == "POST":
-
-        name = request.form["name"]
-        email = request.form["email"]
-        password = request.form["password"]
-        confirm_password = request.form["confirm_password"]
-
-        print("Name:", name)
-        print("Email:", email)
-        print("Password:", password)
-        print("Confirm Password:", confirm_password)
-
-        if password != confirm_password:
-            return "Passwords do not match"
-
-        return "Registration successful!"
-
-    return render_template("register.html")
+# Dynamic route
+@app.route("/order/<order_id>")
+def order(order_id):
+    return render_template("index.html", order_id=order_id)
 
 
 if __name__ == "__main__":
